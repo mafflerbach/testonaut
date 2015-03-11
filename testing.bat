e8ff4c713bb066cc08e2381f37d9d874802b6354
mafflerbach <maren@afflerbach.info>
2015-03-11 21:13:34 +0100

    implement screenshot

--- testing.bat
+++ testing.bat
@@ -4,4 +4,4 @@ java -jar E:\xampp\htdocs\phpSelenium\vendor\selenium\selenium-server.jar -role 
 
 
 java -jar E:\xampp\htdocs\phpSelenium\vendor\selenium\selenium-server.jar -role node  -hub http://localhost:4444/grid/register -Dwebdriver.ie.driver="E:\xampp\htdocs\phpSelenium\vendor\driver\IEDriverServer.exe" -Die.forceCreateProcessApi=true -browser browserName=iexplore ,maxInstances=5,platform=WINDOWS -port 5557
-
+java -jar E:\xampp\htdocs\phpSelenium\vendor\selenium\selenium-server.jar -role node  -hub http://localhost:4444/grid/register -Dwebdriver.chrome.driver="E:\xampp\htdocs\phpSelenium\vendor\driver\chromedriver.exe" -Dwebdriver.ie.driver="E:\xampp\htdocs\phpSelenium\vendor\driver\IEDriverServer.exe" -Die.forceCreateProcessApi=true -nodeConfig E:\xampp\htdocs\phpSelenium\capabilities.json
