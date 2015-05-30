58bf52de0ebddd2157abf8d929a4ea901e100c1f
Maren Afflerbach <maren@afflerbach.info>
2015-05-30 16:29:30 +0200

    remove password protection for installer

--- installer/index.php
+++ installer/index.php
@@ -1,5 +1,5 @@
 <?php
-include 'password.php';
+
 ?>
     <!DOCTYPE html>
     <html>
--- installer/password.php
+++
@@ -1,8 +0,0 @@
-<?php
-$password = "password1234";
-if (!isset($_SERVER['PHP_AUTH_USER']) || $_SERVER['PHP_AUTH_PW'] !== $password)
-{
-    header('WWW-Authenticate: Basic realm="NoConsoleComposer"');
-    header('HTTP/1.0 401 Unauthorized');
-    exit;
-}
