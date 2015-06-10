77f7b38ed92493d8c9059869372f75c782ff7725
Maren Afflerbach <maren@afflerbach.info>
2015-06-10 19:44:16 +0200

    fix image compare

--- lib/testonaut/Selenese/Runner.php
+++ lib/testonaut/Selenese/Runner.php
@@ -10,23 +10,22 @@ class Runner {
 
   /** @var Test */
   public $test;
-
   /** @var string */
   public $hubUrl;
   public $pageUrl;
   public $imagePath;
   private $baseUrl;
-
   private $result = array();
   private $screenshotsAfterEveryStep = FALSE;
   private $screenshotsAfterTest = FALSE;
   private $polling = FALSE;
 
   /**
-   * @param Test $test
+   * @param Test   $test
    * @param string $hubUrl
    */
   public function __construct(Array $test, $hubUrl, $pagePath, $imagePath) {
+
     $this->test = $test;
     $this->hubUrl = $hubUrl;
     $this->pagePath = $pagePath;
@@ -37,18 +36,22 @@ class Runner {
   }
 
   public function result() {
+
     return $this->result;
   }
 
   public function setBaseUrl($baseUrl) {
+
     $this->baseUrl = $baseUrl;
   }
 
   public function getBaseUrl() {
+
     return $this->baseUrl;
   }
 
   protected function _run(Page $content, \DesiredCapabilities $capabilities) {
+
     $browserResult = TRUE;
     $test = new Test();
     $test->setBaseUrl($this->baseUrl);
@@ -70,7 +73,7 @@ class Runner {
     $this->addToPoll($result);
 
     if ($this->screenshotsAfterTest) {
-      $image = $path .'afterTest.png';
+      $image = $path . 'afterTest.png';
       $this->setupImageDir($browserName);
       $this->invokeCommand($image, $webDriver);
     }
@@ -126,6 +129,7 @@ class Runner {
     } catch (\Exception $e) {
       //nothing todo cause session is close
     }
+
     return array(
       'run'           => $res,
       'browserResult' => $browserResult
@@ -138,6 +142,7 @@ class Runner {
    * @return array An array of arrays containing the command and the commandResult
    */
   public function run($capabilities) {
+
     $return = array();
     if (is_array($this->test)) {
       for ($i = 0; $i < count($this->test); $i++) {
@@ -157,47 +162,52 @@ class Runner {
   }
 
   public function screenshotsAfterEveryStep() {
+
     $this->screenshotsAfterEveryStep = TRUE;
   }
 
   public function screenshotsAfterTest() {
+
     $this->screenshotsAfterTest = TRUE;
   }
 
   protected function addToPoll($content) {
+
     $this->writeToFile($this->polling, $content, FILE_APPEND);
   }
 
   protected function invokeCommand($image, $webDriver) {
+
     $screenCommand = new captureEntirePageScreenshot();
-    
+
     $screenCommand->arg1 = $image;
     $screenCommand->runWebDriver($webDriver);
   }
-  
+
   private function setupImageDir($browserName) {
+
     $imageDir = $this->imagePath;
     $path = $imageDir . '/' . $browserName . "/src/";
-    
+
     if (!file_exists($path)) {
       mkdir($path, 0775, TRUE);
       mkdir($imageDir . '/' . $browserName . "/comp/", 0775, TRUE);
       mkdir($imageDir . '/' . $browserName . "/ref/", 0775, TRUE);
-    }  
+    }
   }
-  
 
   protected function captureAndCompare($command, $browserName, $webDriver) {
+
     $this->setupImageDir($browserName);
-    
+
     $imageDir = $this->imagePath;
     $path = $imageDir . '/' . $browserName . "/src/";
-    
+
     $tmp = $command->arg1;
     $command->arg1 = $path . $tmp;
     $this->invokeCommand($command->arg1, $webDriver);
-    
-    if ($comp = $this->compare($browserName, $tmp . '.png')) {
+
+    if ($comp = $this->compare($browserName, $tmp)) {
       $result = "Compare: " . $command->arg1;
       $this->writeToFile($this->polling, $result, FILE_APPEND);
     } else {
@@ -212,6 +222,7 @@ class Runner {
   }
 
   protected function compare($browserName, $imgName) {
+
     $imageDir = $this->imagePath;
     $path = $imageDir . '/' . $browserName . "/src/" . $imgName;
     $pathref = $imageDir . '/' . $browserName . "/ref/" . $imgName;
@@ -223,6 +234,7 @@ class Runner {
       }
       if (class_exists('\\Imagick')) {
         $compare = new Image();
+
         return $compare->compare($path, $pathref, $comp);
       } else {
         return FALSE;
@@ -233,6 +245,7 @@ class Runner {
   }
 
   protected function writeToFile($path, $content, $option = 0) {
+
     file_put_contents($path, $content, $option);
   }
 
