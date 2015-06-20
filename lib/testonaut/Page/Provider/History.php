168e1c8bc1e76757c998f5e1a5cb13a747adf76e
Maren Afflerbach <maren@afflerbach.info>
2015-06-20 22:17:07 +0200

    implement deleting history

--- lib/testonaut/Page/Provider/History.php
+++ lib/testonaut/Page/Provider/History.php
@@ -29,8 +29,13 @@ class History implements ControllerProviderInterface {
     $edit = $app['controllers_factory'];
     $edit->get('/', function (Request $request, $path) use ($app) {
       $this->page = new \testonaut\Page($path);
-        $this->path = $path;
-          
+      $this->path = $path;
+
+      
+      if ($request->query->get('delete')) {
+        $this->deleteHistory($request);
+      }
+
       $browserSettings = new Browser($path);
       $this->browsers = $browserSettings->getSettings();
 
@@ -42,11 +47,37 @@ class History implements ControllerProviderInterface {
       );
       $crumb = new Breadcrumb($path);
       $app['crumb'] = $crumb->getBreadcrumb();
-      
+
       return $app['twig']->render('history.twig');
     });
 
     return $edit;
+  }
+
+  protected function deleteHistory($request) {
+    $db = \testonaut\Config::getInstance()->db;  
+    $dbIns = $db->getInstance();
+    $sql = ''; 
+    if ($request->query->get('all')) {
+      $sql = "delete from history where path=:path";
+      $stm = $dbIns->prepare($sql);
+      $stm->bindParam(':path', $this->path);
+    }
+    if ($request->query->get('browser')) {
+      $browser = $request->query->get('browser');
+      $limit = ''; 
+      if ($request->query->get('count')) {
+        $limit = ' limit '.$request->query->get('count');
+      }
+      $sql = 'delete from history '
+        . 'where date in ('
+        . 'select date from history where browser=:browser and path=:path order by date '.$limit.''
+        . ')';
+      $stm = $dbIns->prepare($sql);
+      $stm->bindParam(':browser', $browser);
+      $stm->bindParam(':path', $this->path);
+    }
+    $stm->execute();
   }
 
   protected function getHistoryList() {
@@ -55,15 +86,15 @@ class History implements ControllerProviderInterface {
     $dbIns = $db->getInstance();
     $sql = "select * from history";
     $stm = $dbIns->prepare($sql);
-    $res =  $stm->execute();
-    
+    $res = $stm->execute();
+
     $foo = array();
     while ($result = $res->fetchArray(SQLITE3_ASSOC)) {
       $date = new \DateTime($result['date']);
       $foo[$result['browser']][$date->format('m.d.Y')][$date->format('H:i:s')]['run'] = $result['run'];
       $foo[$result['browser']][$date->format('m.d.Y')][$date->format('H:i:s')]['result'] = $result['result'];
-    } 
-     return $foo;
- 
+    }
+    return $foo;
   }
+
 }
