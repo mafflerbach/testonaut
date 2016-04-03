dc850677f31fa1fe9ae727fb1a9aac8b1cd204cc
mafflerbach <maren@afflerbach.info>
2016-04-03 21:03:48 +0200

    Fix some issues after refactoring

--- server.php
+++ server.php
@@ -7,8 +7,6 @@ list($type, $data) = explode(';', $data);
 list(, $data)      = explode(',', $data);
 $data = base64_decode($data);
 
-var_dump(str_replace('\\', '\\', $path));
-
 file_put_contents(str_replace('\\', '\\', $path), $data);
 
 
