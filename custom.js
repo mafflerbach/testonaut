681b0b0b40a2c543a93d39567f224fd58232abf1
mafflerbach <maren@afflerbach.info>
2016-06-20 22:21:25 +0200

    'documentation'

--- custom.js
+++ custom.js
@@ -1,16 +1,56 @@
 $(document).ready(function () {
-  $('section div').hide();
 
-  $('h2').click(function() {
-
-    console.log($(this).next('div:hidden').length);
-    console.log($(this).next('div'));
-    if($(this).next('div:hidden').length == 1) {
+  importHTML();
+  buildNavi();
+  //$('section div').hide();
+  $('h2').click(function () {
+    if ($(this).next('div:hidden').length == 1) {
       $(this).next('div:hidden').show();
     } else {
       $(this).next('div').hide();
     }
   })
 
+  $(window).bind('scroll', function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 400) {
+      $('.menu').addClass('fixed');
+    } else {
+      $('.menu').removeClass('fixed');
+    }
+  });
+
 
 })
+
+
+function buildNavi() {
+  if ($("h2[id]").length > 0) {
+  var content = '<div class="menu"><ul>';
+  $("h2[id]").each(function() {
+    content += '<li>';
+    content += '<a href="#'+$(this).attr("id")+'">'+$(this).text()+'</a>';
+    content += '</li>';
+  });
+  content += '<div></ul>';
+
+  $('#menu').append(content);
+  }
+}
+
+function importHTML() {
+  $('import').each(function () {
+    var href = $(this).attr('href');
+    var link = href.split('#');
+    var _this = $(this);
+    $.ajax({
+      type: "GET",
+      url: link[0],
+      dataType: 'html',
+      success: function (data) {
+        var foo = $(data).find('#'+link[1]);
+        _this.replaceWith($(foo.html()));
+      }
+    });
+  })
+}
