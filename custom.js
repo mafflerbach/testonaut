a79e90b339987f99515bffeeedcbb78174626291
mafflerbach <maren@afflerbach.info>
2016-06-21 11:49:22 +0200

    some styles

--- custom.js
+++ custom.js
@@ -2,39 +2,62 @@ $(document).ready(function () {
 
   importHTML();
   buildNavi();
-  //$('section div').hide();
+
   $('h2').click(function () {
     if ($(this).next('div:hidden').length == 1) {
       $(this).next('div:hidden').show();
     } else {
       $(this).next('div').hide();
     }
-  })
+  });
 
   $(window).bind('scroll', function () {
-    console.log($(window).scrollTop());
     if ($(window).scrollTop() > 400) {
       $('.menu').addClass('fixed');
     } else {
       $('.menu').removeClass('fixed');
     }
+
+    if ($(window).scrollTop() > 400) {
+      $('.jumper').fadeIn();
+    } else {
+      $('.jumper').fadeOut();
+    }
   });
 
-
+  window.setTimeout(function() {
+    $('body').fadeIn();
+  }, 200);
 })
 
 
 function buildNavi() {
   if ($("h2[id]").length > 0) {
-  var content = '<div class="menu"><ul>';
-  $("h2[id]").each(function() {
-    content += '<li>';
-    content += '<a href="#'+$(this).attr("id")+'">'+$(this).text()+'</a>';
-    content += '</li>';
-  });
-  content += '<div></ul>';
+    var content = '<div class="menu"><ul>';
+    $("h2[id]").each(function () {
+      content += '<li>';
+      content += '<a href="#' + $(this).attr("id") + '">' + $(this).text() + '</a>';
+      content += '</li>';
+    });
+    content += '<div></ul>';
 
-  $('#menu').append(content);
+    $('#menu').append(content);
+
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      selector = "*[id='"+this.hash.replace('#', '')+"']";
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 400, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+
   }
 }
 
@@ -48,7 +71,7 @@ function importHTML() {
       url: link[0],
       dataType: 'html',
       success: function (data) {
-        var foo = $(data).find('#'+link[1]);
+        var foo = $(data).find('#' + link[1]);
         _this.replaceWith($(foo.html()));
       }
     });
